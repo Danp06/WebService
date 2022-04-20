84d57ee4a0919951a4e5205dae9cb50eaf0e9bae
Danp06 <dgnecco@utb.edu.co>
2022-04-19 21:09:26 -0500

    3 commit

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI, HTTPException
+from itertools import count
+from time import struct_time
+from fastapi import FastAPI, HTTPException, Query
+# from numpy import append, number
 from pydantic import BaseModel
+from typing import Optional
+from datetime import *
+
+# from pytz import HOUR
 
 
 class People(BaseModel):
     id: int
-    cod : str
+    cod: str
     nombre: str
     rol: int
+
+
+class Vehicle(BaseModel):
+    id: str
+    person: People
+    status: bool = False
+    placa: str = "000aaa"
+    fecha_ingreso:  datetime = datetime.now()
+    anotaciones: str = 'ninguna'
 
 
 app = FastAPI(
@@ -14,6 +30,7 @@ app = FastAPI(
 )
 
 Data = []
+vehicles = []
 
 
 @app.get('/', tags=["Home"])
@@ -58,3 +75,64 @@ def update_person(id: int, updatedperson: People):
             Data[index]["rol"] = updatedperson.dict()["rol"]
             return {"message": "Person has been updated succesfully"}
     raise HTTPException(status_code=404, detail="Person not found")
+
+
+@app.get('/register')
+def registers():
+    return vehicles
+
+
+@app.post('/register')
+def create_register(vehicle: Vehicle):
+    vehicles.append(vehicle.dict())
+    return vehicle
+
+
+@app.get('/register/{placa}')
+def get_register(placa: str):
+    vehicles_found = []
+    for register in vehicles:
+        if register['placa'] == placa:
+            vehicles_found.append(register)
+            return vehicles_found
+    raise HTTPException(status_code=404, detail="Vehicle not found")
+
+
+@app.get('/register/')
+def get_register_hours(hour_max: datetime, hour_min: datetime):
+    vehicles_found = []
+    number_vehicles = 0
+    for register in vehicles:
+        if register['fecha_ingreso'] >= hour_min and register['fecha_ingreso'] <= hour_max and \
+                register['status'] == True:
+            vehicles_found.append(register)
+            number_vehicles = number_vehicles + 1
+            return {'number of vehicles:': number_vehicles}
+    raise HTTPException(status_code=422, detail="invalid semantics")
+
+
+@app.put('/register/{placa}/')
+def update_register(placa: str, vehicle: Vehicle):
+    vehicle_found = []
+    for register in vehicles:
+        if register['placa'] == placa and register['status'] == True:
+            vehicles[register] = vehicle
+            vehicle_found.append(vehicle.dict())
+            return {'vehiculo': vehicle.placa, 'información actualizada': vehicle_found}
+    raise HTTPException(status_code=404, detail="Vehicle not found")
+
+
+@app.get('/registre/{rol}')
+def get_vehicles_by_status(rol: int, status = True):
+    number_registers = 0
+    for register in vehicles:
+        for register in vehicles:
+            if register['person']['rol'] == 0 and register['status']==True:
+                number_registers += 1
+            if register['rol'] == 1 and register['status'] == True:
+                number_registers += 1
+            if register['rol'] == 2 and register['status'] == True:
+                number_registers += 1
+            if register['rol'] == 3 and register['status'] == True:
+                number_registers += 1
+            return {'rol:': rol, 'vehiculos parqueados:': number_registers}
