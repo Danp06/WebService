7add16d51a1f866be0eb1e05bd3b2e80100104a5
Danp06 <dgnecco@utb.edu.co>
2022-04-21 13:36:25 -0500

    Final commit

--- app.py
+++ app.py
@@ -10,6 +10,19 @@ from datetime import *
 
 
 class People(BaseModel):
+    """ People constructor object.
+
+        :param id: id of person.
+        :type id: int
+        :param cod: codigo of person.
+        :type cod: str
+        :param name: name of person.
+        :type name: str
+        :param rol: rol of person.
+        :type rol: int
+        :returns: Person object
+        :rtype: object
+    """
     id: int
     cod: str
     nombre: str
@@ -40,17 +53,29 @@ def home():
 
 @app.get('/people', tags=["People"])
 def list_people():
+    """ Retorna objeto personas en str.
+          :returns: object person
+          :rtype: str
+    """
     return Data
 
 
 @app.post('/people', tags=["People"])
 def create_people(person: People):
+    """ Funcion para crear y retornar personas en str.
+            :returns: Person object.
+            :rtype: str.
+    """
     Data.append(person.dict())
     return Data[-1]
 
 
 @app.get('/people/{id}', tags=["People"])
 def find_people(id: int):
+    """ Funcion para buscar y retornar una persona en str.
+            :returns: Person object.
+            :rtype: str.
+    """
     for person in Data:
         if person["id"] == id:
             return person
@@ -59,6 +84,10 @@ def find_people(id: int):
 
 @app.delete('/people/{id}', tags=["People"])
 def delete_people(id: int):
+    """ Funcion para eliminar personas.
+            :returns: Person object.
+            :rtype: str.
+    """
     for index, person in enumerate(Data):
         if person["id"] == id:
             Data.pop(index)
@@ -68,6 +97,10 @@ def delete_people(id: int):
 
 @app.put('/people/{id}', tags=["People"])
 def update_person(id: int, updatedperson: People):
+    """ Funcion para actualizar y retornar personas en str.
+            :returns: Person object.
+            :rtype: str.
+    """
     for index, person in enumerate(Data):
         if person["id"] == id:
             Data[index]["id"] = id
@@ -79,17 +112,29 @@ def update_person(id: int, updatedperson: People):
 
 @app.get('/register', tags=["Vehicle"])
 def registers():
+    """ Retorna objeto vehicle en str.
+          :returns: object vehicle
+          :rtype: str
+    """
     return vehicles
 
 
 @app.post('/register', tags=["Vehicle"])
 def create_register(vehicle: Vehicle):
+    """ Funcion para crear y retornar vehicle en str.
+            :returns: vehicle object.
+            :rtype: str.
+    """
     vehicles.append(vehicle.dict())
     return vehicle
 
 
 @app.get('/register/{placa}', tags=["Vehicle"])
 def get_register(placa: str):
+    """ Funcion para buscar y retornar un vehicle en str.
+            :returns: vehicle object.
+            :rtype: str.
+    """
     vehicles_found = []
     for register in vehicles:
         if register['placa'] == placa:
@@ -100,11 +145,15 @@ def get_register(placa: str):
 
 @app.get('/register/', tags=["Vehicle"])
 def get_register_hours(hour_max: datetime, hour_min: datetime):
+    """ Funcion para buscar un vehicle a una hora y retornar cantidad de vehiculos.
+            :returns: cantidad vehiculos.
+            :rtype: int.
+    """
     vehicles_found = []
     number_vehicles = 0
     for register in vehicles:
         if register['fecha_ingreso'] >= hour_min and register['fecha_ingreso'] <= hour_max and \
-                register['status'] == True:
+                register['status'] is True:
             vehicles_found.append(register)
             number_vehicles = number_vehicles + 1
             return {'number of vehicles:': number_vehicles}
@@ -113,9 +162,13 @@ def get_register_hours(hour_max: datetime, hour_min: datetime):
 
 @app.put('/register/{placa}/', tags=["Vehicle"])
 def update_register(placa: str, vehicle: Vehicle):
+    """ Funcion para actualizar y retornar vehicle en str.
+            :returns: vehicle object.
+            :rtype: str.
+    """
     vehicle_found = []
     for register in vehicles:
-        if register['placa'] == placa and register['status'] == True:
+        if register['placa'] == placa and register['status'] is True:
             vehicles[register] = vehicle
             vehicle_found.append(vehicle.dict())
             return {'vehiculo': vehicle.placa, 'información actualizada': vehicle_found}
@@ -123,19 +176,23 @@ def update_register(placa: str, vehicle: Vehicle):
 
 
 @app.get('/registre/{rol}', tags=["Vehicle"])
-def get_vehicles_by_status(rol: int, status = True):
+def get_vehicles_by_status(rol: int):
+    """ Funcion para buscar un vehiculos parqueados y retornar cantidad de vehiculos por rol.
+            :returns: cantidad vehiculos por rol.
+            :rtype: int.
+    """
     number_registers = [0, 0, 0, 0]
     for register in vehicles:
-        if register['person']['rol'] == 0 and register['status']==True:
+        if register['person']['rol'] == 0 and register['status'] is True:
             x = number_registers[0]
             number_registers.append(x+1)
-        if register['person']['rol'] == 1 and register['status'] == True:
+        if register['person']['rol'] == 1 and register['status'] is True:
             x = number_registers[1]
             number_registers.append(x+1)
-        if register['person']['rol'] == 2 and register['status'] == True:
+        if register['person']['rol'] == 2 and register['status'] is True:
             x = number_registers[2]
             number_registers.append(x+1)
-        if register['person']['rol'] == 3 and register['status'] == True:
+        if register['person']['rol'] == 3 and register['status'] is True:
             x = number_registers[3]
             number_registers.append(x+1)
         return {'rol:': rol, 'vehiculos parqueados:': number_registers[rol]}
